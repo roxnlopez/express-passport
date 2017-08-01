65d099e3db3f9e0ef1e25a378f4d07c3649a3244
roxnlopez <roxnlopez@gmail.com>
2017-08-01 17:06:39 -0600

    added login

--- starter-code/controllers/users.js
+++ starter-code/controllers/users.js
@@ -1,28 +1,36 @@
 var passport = require("passport");
 
 // GET /signup
-function getSignup(request, response) {
+function getSignup(request, response, next) {
+	response.render('signup.ejs', {message: request.flash('signupMessage')});
 }
 
 // POST /signup
 function postSignup(request, response, next) {
 	//save a new user
-
 	let signupStrategy = passport.authenticate('local-signup', {
 		successRedirect: '/',
-		failureRedirect: 'signup',
+		failureRedirect: '/signup',
 		failureFlash: true
 	});
-	console.log("YO");
+	// console.log("YO");
 	return signupStrategy(request, response, next);
 }
 
 // GET /login
-function getLogin(request, response) { 
+function getLogin(request, response, next) {
+	response.render('login.ejs', {message: request.flash('loginMessage')}); 
 }
 
 // POST /login 
-function postLogin(request, response) {
+function postLogin(request, response, next) {
+	//user login
+	let loginStrategy = passport.authenticate('local-login', {
+      successRedirect : '/',
+      failureRedirect : '/login',
+      failureFlash : true
+    });
+    return loginStrategy(request, response, next);
 }
 
 // GET /logout
